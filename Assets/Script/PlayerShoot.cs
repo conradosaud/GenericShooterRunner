e41daac6b372055045984d7b7db396926c7073a8
Conrado Saud <50385963+conradosaud@users.noreply.github.com>
2024-03-28 13:57:06 -0300

    Tiros

--- Assets/Script/PlayerShoot.cs
+++ Assets/Script/PlayerShoot.cs
@@ -6,16 +6,18 @@ public class PlayerShoot : MonoBehaviour
 {
 
     public Transform prefabTiro;
+    public Transform containerTiros;
     public float intervaloTiro = 0.5f;
 
     void Start()
     {
+        containerTiros = GameObject.Find("ContainerTiros").transform;
         Atirar();
     }
 
     void Atirar()
     {
-        Transform instancia = Instantiate(prefabTiro);
+        Transform instancia = Instantiate(prefabTiro, containerTiros);
         instancia.position = transform.position;
         instancia.rotation = transform.rotation;
         Invoke("Atirar", intervaloTiro);
--- Assets/Script/Tiro.cs
+++ Assets/Script/Tiro.cs
@@ -6,10 +6,22 @@ public class Tiro : MonoBehaviour
 {
 
     public float velocidade = 5f;
+    public float tempoDestruir = 3.5f;
+
+    private void Start()
+    {
+        Invoke("Destruir", tempoDestruir);
+    }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate( -(velocidade * Time.deltaTime) ,0,0);
     }
+
+    void Destruir()
+    {
+        Destroy(gameObject);
+    }
+
 }
